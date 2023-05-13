3ce70de44a6aba4a03a21e591cca3fc1cd6c1c3b
H Del Col <h.delcol@outlook.com>
2023-05-13 17:09:23 -0300

    programa atualizado, agora o programa gera um numero aleatoria que deve ser replicado

--- kids_math.c
+++ kids_math.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int i;
+    int numero_aleatorio, resposta;
     char escolha;
 
-    printf("Escolha a opcao de contagem (C para cresente, D para decresente): ");
+    srand(time(NULL));
+
+    printf("Digite 'S' para comecar o jogo: ");
     scanf("%c", &escolha);
 
-    if(escolha == 'C' || escolha == 'c') {
-        for( i = 0; i <= 1000; i++) {
-            printf("%d\n", i);
-        }
-    } else if (escolha == 'D' || escolha == 'd') {
-        for(i = 1000; i >= 0; i--) {
-            printf("%d\n", i);
+    if(escolha == 'S' || escolha == 's') {
+        numero_aleatorio = rand() % 1001;
+        printf("Repita o numero gerado: ");
+        scanf("%d", &resposta);
+
+        if(resposta == numero_aleatorio) {
+            printf("Parabens, voce acertou!\n");
+        } else {
+            printf("Que pena, voce errou! o numero correto era %d.\n", numero_aleatorio);
         }
     } else {
         printf("Opcao invalida!\n");
